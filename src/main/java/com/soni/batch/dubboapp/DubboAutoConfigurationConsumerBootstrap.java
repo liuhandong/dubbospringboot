4aa02a1ac321ac7a0404e46e8da2e80d0322c94e
handong.liu <handong.liu@DLC000D7N3GD2DD.dir.svc.accenture.com>
2019-04-09 19:28:55 +0800

    add files

---
+++ src/main/java/com/soni/batch/dubboapp/DubboAutoConfigurationConsumerBootstrap.java
@@ -0,0 +1,15 @@
+package com.soni.batch.dubboapp;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class DubboAutoConfigurationConsumerBootstrap 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
