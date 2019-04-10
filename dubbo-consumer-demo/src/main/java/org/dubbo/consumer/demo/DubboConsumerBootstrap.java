7e484c7fb7c4475bc8ba5a2623c3665a492d2b3e
liuhandong <liuhandong11@sina.com>
2019-04-11 00:03:06 +0800

    整合nacos、cloud 以及dubbo-spring-boot-starter version2.7.1-> 0.2.1.RELEASE

--- dubbo-consumer-demo/src/main/java/org/dubbo/consumer/demo/DubboConsumerBootstrap.java
+++ dubbo-consumer-demo/src/main/java/org/dubbo/consumer/demo/DubboConsumerBootstrap.java
@@ -1,19 +1,24 @@
 package org.dubbo.consumer.demo;
 
-import org.apache.dubbo.config.annotation.Reference;
 import org.dubbo.common.service.DemoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 
 
 /**
- * Hello world!
+ * 
+ * @author ThinkPad
  *
  */
+@SpringBootApplication
+@EnableAutoConfiguration
 public class DubboConsumerBootstrap 
 {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
--- dubbo-provider-demo/src/main/java/org/dubbo/provider/demo/DubboProviderDemo.java
+++ dubbo-provider-demo/src/main/java/org/dubbo/provider/demo/DubboProviderDemo.java
@@ -2,12 +2,19 @@ package org.dubbo.provider.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 
 /**
- * Hello world!
+ * https://github.com/alibaba/nacos
+ * https://blog.csdn.net/u014677702/article/details/85888590
+ * @author ThinkPad
  *
  */
+@SpringBootApplication
 @EnableAutoConfiguration
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class DubboProviderDemo 
 {
     public static void main( String[] args )
